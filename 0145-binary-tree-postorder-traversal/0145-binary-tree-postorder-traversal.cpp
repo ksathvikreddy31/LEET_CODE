0681a2905a245b1e38e9ad05bd431a8cdc9850c3
SATHVIK REDDY KANTAREDDY <161666456+ksathvikreddy31@users.noreply.github.com>
2025-04-14 12:51:15 +0530

    Time: 0 ms (100%), Space: 10.8 MB (98.79%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,36 +1,29 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        if (!root) return {}; 
-        stack<TreeNode*>st1;
-        stack<TreeNode*>st2;
-        vector<int>vec;
-        TreeNode *node=root;
-        st1.push(node);
-        while(!st1.empty())
-        {   
-            node=st1.top();
-            st1.pop();
-            if(node->left!=NULL) st1.push(node->left);
-            if(node->right!=NULL) st1.push(node->right);
-            st2.push(node);  
+        TreeNode *curr = root;
+        vector<int> vec;
+        if (root == NULL) return vec;
+
+        stack<TreeNode*> st;
+        TreeNode *lastVisited = nullptr;
+
+        while (!st.empty() || curr != NULL) {
+            if (curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            } else {
+                TreeNode *peekNode = st.top();
+                if (peekNode->right != NULL && lastVisited != peekNode->right) {
+                    curr = peekNode->right;
+                } else {
+                    vec.push_back(peekNode->val);
+                    lastVisited = peekNode;
+                    st.pop(); 
+                }
+            }
         }
-        while(!st2.empty())
-        {
-            vec.push_back(st2.top()->val);
-            st2.pop();
-        }
+
         return vec;
     }
 };
