ba15bbe37d30cbb2fff1e8d515cb47d08ec4602b
SATHVIK REDDY KANTAREDDY <161666456+ksathvikreddy31@users.noreply.github.com>
2025-09-21 12:33:09 +0530

    Time: 0 ms (100%), Space: 9.7 MB (27.29%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,55 @@
+#include <stack>
+using namespace std;
+
+class MyQueue {
+public:
+    stack<int> st1, st2;
+
+    MyQueue() {
+        
+    }
+    
+    // Push element x to the back of queue
+    void push(int x) {
+        st1.push(x);
+    }
+    
+    // Removes the element from in front of queue and returns it
+    int pop() {
+        // Transfer elements if st2 is empty
+        if(st2.empty()) {
+            while(!st1.empty()) {
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        int val = st2.top();
+        st2.pop();
+        return val;
+    }
+    
+    // Get the front element
+    int peek() {
+        if(st2.empty()) {
+            while(!st1.empty()) {
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        return st2.top();
+    }
+    
+    // Returns whether the queue is empty
+    bool empty() {
+        return st1.empty() && st2.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
